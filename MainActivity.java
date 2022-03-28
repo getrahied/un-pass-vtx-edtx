0b3fa7675c9d49500f56e554d4f21c6fb76e9cb7
DevSufy <95271348+getrahied@users.noreply.github.com>
2022-03-28 21:50:56 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,10 +1,21 @@
 package com.example.devsufy;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void display (View view) {
+        EditText username = (EditText)findViewById(R.id.username);
+        EditText password = (EditText)findViewById(R.id.password);
+
+        String details = "Username: "+username.getText().toString()+"\n"+"Password: "+password.getText().toString();
+
+        Toast.makeText(this, details, Toast.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
